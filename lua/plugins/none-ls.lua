17521629205a20d3866186ac5c8c8734b781653b
barrage <damienboyer10@gmail.com>
2024-11-29 14:59:29 +0100

    Commit

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,15 +5,29 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua, -- lua
-				null_ls.builtins.formatting.clang_format, -- c & java
+				null_ls.builtins.formatting.clang_format.with({
+					filetypes = { "c", "java" },
+				}), -- c & java
 				null_ls.builtins.formatting.google_java_format, -- java
-				null_ls.builtins.diagnostics.checkstyle,
-				--   .with({
-				--   extra_args = { "-c", "/sun_checks.xml" }, -- or "/sun_checks.xml" or path to self written rules
-				-- }),                                         -- java
 				null_ls.builtins.formatting.prettier,
+
+				null_ls.builtins.diagnostics.checkstyle, -- java
 				-- null_ls.builtins.diagnostics.cpplint, -- c
+
+				-- null_ls.builtins.formatting.ast_grep, -- c
+				-- null_ls.builtins.diagnostics.ast_grep, -- c
 			},
+			-- Add the format on save functionality
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
